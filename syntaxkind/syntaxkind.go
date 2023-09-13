6da6097311000cb5fc24941da47cb2ae463276e1
Eduardo Ricardo <eduwric@gmail.com>
2023-09-13 10:45:49 -0300

    feat: :sparkles: create a simple lexer

---
+++ syntaxkind/syntaxkind.go
@@ -0,0 +1,12 @@
+package syntaxkind
+
+// TODO: use enumerable instead of strings in the future
+const (
+	EnfOfFileToken  = "EndOfFileToken"
+	WhiteSpaceToken = "WhiteSpaceToken"
+	NumberToken     = "NumberToken"
+	PlusToken       = "PlusToken"
+	MinusToken      = "MinusToken"
+	StartToken      = "StartToken"
+	SlashToken      = "SlashToken"
+)
