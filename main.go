abf12c749f1207eb84fe0f72247d67f7652d4f74
Eduardo Ricardo <eduwric@gmail.com>
2023-09-13 08:36:26 -0300

    feat: :sparkles: read line

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
 func main() {
 	println("Hello gompiler")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		print(">")
+		scanner.Scan()
+		line := scanner.Text()
+
+		println(line)
+	}
 }
